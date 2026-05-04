b0c4d9fb641b0af05ad0797920f27cc87a81fd94
srhoneAI <seanrhone@Seans-iMac.local>
2026-05-04 14:45:50 -0400

    AB#254 | Add CoffeeSize column to Customer table

--- Customer.sql
+++ Customer.sql
@@ -2,5 +2,6 @@ CREATE TABLE [dbo].Customer(
   [ID] Int IDENTITY(1,1) NOT NULL,
   [CustomerID] [VarChar](100), NOT NULL,
   [CustomerName] [VarChar](200), NOT NULL,
-  [CoffeeType] [VarChar](100), NOT NULL
+  [CoffeeType] [VarChar](100), NOT NULL,
+  [CoffeeSize] [VarChar](50), NOT NULL
 )
